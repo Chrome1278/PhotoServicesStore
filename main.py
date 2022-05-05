da343f1bfcb86f4fcfa727530736fa75fdb81123
Chrome1278 <63706125+Chrome1278@users.noreply.github.com>
2022-05-05 22:34:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# import inputs
+# from sqlalchemy import create_engine
+#
+#
+# def main():
+#     print("----- Вы вошли в интерфейс базы данных Салона Фотоуслуг! -----",)
+#     try:
+#         engine = create_engine('postgresql+psycopg2://postgres:1278@localhost/postgres')
+#         inputs.output_info(engine)
+#     except:
+#         print("Ошибка подключения к базе данных!")
+#
+#
+# main()
+
+#
+import streamlit as st
+
+
+st.write("""# HELLO World""")
