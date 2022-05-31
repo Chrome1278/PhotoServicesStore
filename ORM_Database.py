5ac980b45b90dd3a7ff2e3596cfc133ca4c2d0c3
Chrome1278 <63706125+Chrome1278@users.noreply.github.com>
2022-05-31 22:40:42 +0300

    Update

--- ORM_Database.py
+++ ORM_Database.py
@@ -1,17 +1,10 @@
-from sqlalchemy import create_engine, MetaData, Table, Integer, String, Numeric, Column, DateTime, ForeignKey
-# from sqlalchemy.dialects.postgresql import psycopg2
+from sqlalchemy import create_engine, Integer, String, Column, DateTime, ForeignKey
 import psycopg2
-from sqlalchemy.sql import select, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy.orm import Session, sessionmaker, query
-from datetime import datetime
 from sqlalchemy.sql import func
 
-# session = Session(bind=engine)
 Base = declarative_base()
-# Session = sessionmaker(bind=engine)
-# session = Session()
 
 
 class BranchOffice(Base):
@@ -40,10 +33,8 @@ class Employee(Base):
     phone_number = Column(String(18), nullable=False)
     e_mail = Column(String(80), nullable=False)
     branch_office_id = Column(Integer(), ForeignKey("branch_office.branch_office_id"), nullable=False)
-    # branch_office = relationship("BranchOffice")
     purchase = relationship('Purchase')
     service = relationship('Service')
-    # services_list = relationship('ServicesList')
 
 
 class EmployeeLogInfo(Base):
@@ -57,13 +48,10 @@ class EmployeeLogInfo(Base):
 class Purchase(Base):
     __tablename__ = 'purchase'
     purchase_number = Column(Integer(), primary_key=True)
-    purchase_date = Column(DateTime(), nullable=False, server_default=func.now()) #возможна проблема с датой и временем
+    purchase_date = Column(DateTime(), nullable=False, server_default=func.now())
     customer_id = Column(Integer(), ForeignKey("customer.customer_id"), nullable=False)
-    # customer = relationship('Customer')
     employee_code = Column(String(4), ForeignKey("employee.employee_code"), nullable=False)
-    # employee = relationship('Employee')
     branch_office_id = Column(Integer(), ForeignKey("branch_office.branch_office_id"), nullable=False)
-    # branch_office = relationship('BranchOffice')
     purchase_amount = Column(Integer(), nullable=True, default=0)
     services_list = relationship('ServicesList')
 
@@ -74,8 +62,7 @@ class Service(Base):
     service_name = Column(String(80), nullable=False)
     service_price = Column(Integer(), nullable=False)
     employee_code = Column(String(4), ForeignKey("employee.employee_code"), nullable=False)
-    services_list = relationship('ServicesList')  ###
-    # employee = relationship('Employee')
+    services_list = relationship('ServicesList')
 
 
 class ServicesList(Base):
@@ -83,17 +70,10 @@ class ServicesList(Base):
     services_list_number = Column(Integer(), primary_key=True)
     services_amount = Column(Integer(), nullable=False)
     service_code = Column(String(4), ForeignKey("service.service_code"), nullable=False)
-    # service = relationship('Employee')
     purchase_number = Column(Integer(), ForeignKey("purchase.purchase_number"), nullable=False)
-    # purchase = relationship('Purchase')
 
 
 if __name__ == '__main__':
     engine = create_engine('postgresql+psycopg2://postgres:1278@localhost/postgres')
     Base.metadata.create_all(engine)
-
-# print(engine)
-
-
-
-
+    
