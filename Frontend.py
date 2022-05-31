f0281aad57cd5c760cca2f642c4fc3aa56267e21
Chrome1278 <63706125+Chrome1278@users.noreply.github.com>
2022-05-31 22:19:52 +0300

    Update

--- Frontend.py
+++ Frontend.py
@@ -1,13 +1,6 @@
 import streamlit as st
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, query
-from src.ORM_Database import BranchOffice, Customer, Employee, EmployeeLogInfo, Purchase, Service, ServicesList
 from src.Data_listing import all_services, all_offices, get_service_price, is_new_customer, add_new_customer, \
     get_customer_id, get_random_employee_code, get_branch_office_id, add_new_purchase
-
-#engine = create_engine('postgresql+psycopg2://postgres:1278@localhost/postgres')
-#Session = sessionmaker(bind=engine)
-#session = Session()
 
 
 def app():
@@ -24,19 +17,10 @@ def app():
             self.services_amount = []
             self.services_price = []
 
-    # Делаем синглтон с флагом на отключение логинга в условие после срабатывание флага на авторизациюк
-
     @st.experimental_singleton
     class Login:
         def __init__(self):
-            # self.full_name = col_login_1.text_input("Full name")
-            # self.phone_number = col_login_2.text_input("Phone number")
-            # self.e_mail = col_login_3.text_input("Email")
             self.is_login = False
-
-
-
-
 
     st.markdown("<h1 style='text-align: center;"
                 "color: #03fc98;"
@@ -44,28 +28,35 @@ def app():
 
     container = st.container()
     col_login_0, col_login_1, col_login_2, col_login_3, col_login_4 = container.columns((2, 3, 3, 3, 2))
+    _, col_login, col_signup, _ = container.columns((8, 4, 3, 8))
 
     full_name = col_login_1.text_input("Full name")
     phone_number = col_login_2.text_input("Phone number")
     e_mail = col_login_3.text_input("Email")
 
-
-    if col_login_2.button("Авторизироваться!"):
-        if not is_new_customer(full_name, phone_number, e_mail):
-            add_new_customer(full_name, phone_number, e_mail)
-            container.info(f"Добро пожаловать, {full_name}!")
+    if col_login.button("Авторизироваться"):
+        if full_name == '' or phone_number == '' or e_mail == '':
+            container.warning(f"Вы ввели не все данные!")
+        elif not is_new_customer(full_name, phone_number, e_mail):
+            container.warning(f"Вы ввели неверные данные для входа в систему!")
         else:
             container.info(f"Рады вас снова видеть, {full_name}!")
-        Login().is_login = True
+            Login().is_login = True
+
+    if col_signup.button("Зарегистрироваться"):
+        if full_name == '' or phone_number == '' or e_mail == '':
+            container.warning(f"Вы ввели не все данные!")
+        elif is_new_customer(full_name, phone_number, e_mail):
+            container.warning(f"Такой пользователь уже существует! Попробуйте авторизоваться.")
+        else:
+            add_new_customer(full_name, phone_number, e_mail)
+            container.info(f"Добро пожаловать, {full_name}!")
+            Login().is_login = True
 
     if Login().is_login:
         customer_id = get_customer_id(full_name, phone_number, e_mail)
 
         container.markdown("---")
-
-
-
-
 
         _, col_l, _, col_r, col_del, _ = container.columns((2, 6, 3, 6, 1, 2))
         branch_office = col_l.selectbox("Выбор филлиала:", all_offices)
@@ -84,7 +75,7 @@ def app():
             for item in Total().services_list:
                 if item == service:
                     container.warning("Услуга уже добавлена! Если хотите изменить количество выбранной услуги,"
-                               " то необходимо сначала её удалить.")
+                                      " то необходимо сначала её удалить.")
                     added_warning = True
                     break
             if not added_warning:
@@ -96,7 +87,6 @@ def app():
                 print(Total().services_price)
         purchase_amount = 0
 
-
         for service, service_amount, service_price in zip(Total().services_list, Total().services_amount,
                                                           Total().services_price):
             purchase_amount += service_price
@@ -107,7 +97,8 @@ def app():
             font-weight: bold;
             }
           </style>"""
-                           f""" {Total().services_list.index(service) + 1}. "{service}", {service_amount}шт, всего {service_price}""",
+                           f""" {Total().services_list.index(service) + 1}."""
+                           f"""{service}" - {service_amount} за {service_price}""",
                            unsafe_allow_html=True)
 
             if col_del.button('DELETE', key=str(service)):
@@ -124,12 +115,9 @@ def app():
         branch_office_id = get_branch_office_id(branch_office)
         employee_code = get_random_employee_code(branch_office_id)
 
-
         if accept_purchase and branch_office:  # and other
             try:
                 add_new_purchase(customer_id, employee_code, branch_office_id, purchase_amount)
                 st.success(f"Заказ оформлен! Спасибо что выбрали нас, {full_name}!")
             except Exception as e:
                 st.error(f"Ошибка офорлмения заказа! Подробнее: {e}")
-
-app()
